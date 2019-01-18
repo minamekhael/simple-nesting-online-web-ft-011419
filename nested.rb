2c00769a21b01adfe99dfab478eec4bd2c30383c
Mina Mekhael <mina.mekhael@ymail.com>
2019-01-18 21:52:55 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -77,11 +77,11 @@ def adding_matz
           :languages => ["C"]
         }
      }
- programmer_hash[:yukihiro_matsumoto] = {
-    known_for: "Ruby",
-    languages:
-      ["LISP", "C"]
+      programmer_hash[:yukihiro_matsumoto] = {
+        known_for: "Ruby",
+        languages:["LISP", "C"]
     }
+    
     programmer_hash
     
 end
@@ -124,6 +124,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-    programmer_hash[:dennis_ritchie][:languages] = "GUI"
+    programmer_hash[:dennis_ritchie][:languages] = 
     programmer_hash
 end
