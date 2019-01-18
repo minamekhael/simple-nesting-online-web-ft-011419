b1fbcc7acb21b43d3e5a1ac3d5751f8476dcf3f2
Mina Mekhael <mina.mekhael@ymail.com>
2019-01-18 21:49:33 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -102,8 +102,9 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
+     #change what Alan Kay is :known_for to the value of the alans_new_info variable.
+     
+    programmer_hash[:alan_kay] alans_new_info = "GUI"
      
      
 end
