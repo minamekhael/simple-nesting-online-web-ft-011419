327716e4e47073acfb667d412007aab145c7cc9e
Mina Mekhael <mina.mekhael@ymail.com>
2019-01-18 21:51:06 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -102,10 +102,9 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      
     programmer_hash[:alan_kay][:known_for] = "GUI"
-     programmer_hash
+    programmer_hash
      
 end
 
@@ -125,5 +124,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+    programmer_hash[:dennis_ritchie][:languages] = "GUI"
+    programmer_hash
 end
