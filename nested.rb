6e5b26e6f0416e20d51a95ee3b0e202d4e26afaa
Mina Mekhael <mina.mekhael@ymail.com>
2019-01-18 21:55:14 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -124,6 +124,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-    programmer_hash[:denn][:known_for] = "GUI"
+    programmer_hash[:dennis_ritchie][:languages] << "Assembly"
     programmer_hash
 end
